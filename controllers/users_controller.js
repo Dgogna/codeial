b1869861b3f15d450fbfe1736d419924ade2d846
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-13 17:33:53 +0530

    Added user profile router

---
+++ controllers/users_controller.js
@@ -0,0 +1,4 @@
+
+module.exports.profile=function(req,res){
+    return res.send("<h1>User Profile route</h1>")
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 
 const express=require("express");
+const { appendFile } = require("fs");
 
 const router=express.Router();
 const homeController=require("../controllers/home_controller");
 
+
 router.get("/",homeController.home);
+router.use("/users",require("./users"));
 
 console.log("router added succesfully");
 
