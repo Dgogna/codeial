235dfb84f239e223183601bd83997f576f1cd0dd
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-13 23:02:24 +0530

    added ejs as the view engine

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port=3000;
 // use express router
 app.use("/",require("./routes/index"));
 
+// set up view engine
+app.set("view engine","ejs");
+app.set("views","./views");
 
 
 app.listen(port,function(err){
