dc29e8acc2076e52f224b802f46e41faa75d42bd
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-14 23:49:35 +0530

    created the user sign up authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user");
 
 module.exports.profile=function(req,res){
     return res.render("user-profile",{
@@ -23,6 +24,30 @@ module.exports.signIn=function(req,res){
 
 module.exports.create=function(req,res){
     // todo later
+
+    if(req.body.password != req.body.confirm_password){
+        // console.log("pass not matched");
+        return res.redirect("back");
+    }
+
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log("error in finding user in signing up");
+            return ;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log("error in creating the user");
+                    return ;
+                }
+                return res.redirect("/users/sign-in")
+            })
+        }else{
+            return res.redirect("back");
+        }
+    })
+
 }
 
 // get the sign in data
