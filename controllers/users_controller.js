bdcd3c289ec2610a1b252f7750bf2dc794e46e63
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-14 22:02:33 +0530

    learned about cookie

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,13 +8,25 @@ module.exports.profile=function(req,res){
 // for sign up 
 module.exports.signUp=function(req,res){
     return res.render("user_sign_up",{
-        title:"SignUP"
+        title:"Sign up"
     })
 };
 
 // for sign in
 module.exports.signIn=function(req,res){
     return res.render("user_sign_in",{
-        title:"SignIN"
+        title:"Sign in"
     })
 };
+
+// for signing up or to create a new user
+
+module.exports.create=function(req,res){
+    // todo later
+}
+
+// get the sign in data
+
+module.exports.createSession=function(req,res){
+    // todo later
+}
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 const express=require("express");
+var cookieParser = require('cookie-parser')
 const app=express();
 const port=3000;
+const db=require("./config/mongoose");
 
+
+app.use(cookieParser())
+app.use(express.static("assets"));
+app.use(express.urlencoded({extended:true}));
 // use express router
 app.use("/",require("./routes/index"));
-
 // set up view engine
 app.set("view engine","ejs");
 app.set("views","./views");
-
 
 app.listen(port,function(err){
     if(err){
