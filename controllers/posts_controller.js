2c8a21e20ac8c8c1af6f999ade30921d89a28d32
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-19 15:22:38 +0530

    created the post controller and the routes to tract the posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,14 @@
+const Post=require("../models/post");
 
-module.exports.action=function(req,res){
-    return res.render("user-profile",{
-        title:"codeial/Profile"
+module.exports.create =function(req,res){
+    Post.create({
+        content:req.body.content,
+        user:req.user._id
+    },function(err,post){
+        if(err){
+            console.log("error in posting the post");
+            return ;
+        }
+        return res.redirect("/");
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ const express=require("express");
 
 const router=express.Router();
 const homeController=require("../controllers/home_controller");
-const postsController=require("../controllers/posts_controller");
 
 
 router.get("/",homeController.home);
-router.get("/action",postsController.action);
 
 router.use("/users",require("./users"));
+router.use("/posts",require("./posts"));
 
 console.log("router added succesfully");
 
