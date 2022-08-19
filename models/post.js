f915e505abf74c47026d4e073d0963919dd010b5
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-19 18:14:24 +0530

    added the comments schema

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema=new mongoose.Schema({
    user:{
     type:mongoose.Schema.Types.ObjectId,
     ref:"User"
-   }
+   },
+//    include the array of id's of all comments in the post schema
+   comments:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+    }
+   ]
 },{
     timestamps:true
 });
