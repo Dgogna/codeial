7b4c8fb54fa440805e873375cbf59bb284422aef
Dhruv Gogna <dhruvgogna01@gmail.com>
2022-08-20 23:17:17 +0530

    added the delete functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -16,3 +16,18 @@ module.exports.create=function(req,res){
         }
     })
 }
+
+module.exports.destroy=function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user==req.user.id){
+            const post_id=comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(post_id,{$pull:{comments:req.params.id}},function(err,post){
+                return res.redirect("back");
+            });
+        }
+        else{
+            res.redirect("back");
+        }
+    })
+}
